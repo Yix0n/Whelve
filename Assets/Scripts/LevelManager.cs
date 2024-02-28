7f31491324d75218420e2bea741b2b99a2f0c840
Wiktor Słodkowski <2473954@teb-gdansk.ad>
2024-02-28 12:43:28 +0100

    28.02.2024

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    Transform player;
+    public GameObject basherPrefab;
+    public float spawnInterval = 1;
+    float timeSinceSpawn;
+    float spawnDistance = 30;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        timeSinceSpawn = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceSpawn += Time.deltaTime;
+
+        if (timeSinceSpawn > spawnInterval)
+        {
+            Vector2 random = Random.insideUnitCircle.normalized;
+
+            Vector3 randomPosition = new Vector3(random.x, 0, random.y);
+
+
+            randomPosition *= spawnDistance;
+
+            randomPosition += player.position;
+
+            if (!Physics.CheckSphere(new Vector3(randomPosition.x, 1, randomPosition.z), 0.5f))
+            {
+
+                Instantiate(basherPrefab, randomPosition, Quaternion.identity);
+
+                timeSinceSpawn = 0;
+            }
+
+        }
+
+        //TODO: przyśpieszyć resp over time
+
+
+    }
+}
